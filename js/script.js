955778b3be0ad860aa8cdd346cacf934be9da0a6
MicheleRai <rainonemichele1999@gmail.com>
2022-11-07 16:49:14 +0100

    array and photo added

--- js/script.js
+++ js/script.js
@@ -10,3 +10,43 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede*/
+
+/*Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
+
+const arrTeam = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    },
+];
